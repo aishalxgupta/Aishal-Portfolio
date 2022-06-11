4e928731fe38b48d750834c73dfd1b08908f7294
Aishal Gupta <aishalgupta3008@gmail.com>
2022-06-11 08:59:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import { Route } from "react-router-dom"
+// import { Switch } from "react-router-dom"
+import { Route, Switch, useLocation } from "react-router"
+import { ThemeProvider } from "styled-components"
+import { DarkTheme,lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
+
+
+import Main from "./components/Main"
+import AboutPage from "./components/AboutPage"
+import BlogPage  from "./components/BlogPage"
+import WorkPage from "./components/WorkPage"
+import MySkillsPage from "./components/MySkillsPage"
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar"
+
+function App() {
+
+  const location = useLocation();
+  return <>
+
+  <GlobalStyle/>
+
+  <ThemeProvider theme={lightTheme}>
+    <SoundBar />
+
+  <AnimatePresence exitBeforeEnter>
+  <Switch location={location} key={location.pathname}>
+    <Route exact path="/" component={Main}/>
+    <Route exact path="/about" component={AboutPage}/>
+    <Route exact path="/blog" component={BlogPage}/>
+    <Route exact path="/work" component={WorkPage}/>
+    <Route exact path="/skills" component={MySkillsPage}/>
+
+  </Switch>
+  </AnimatePresence>
+  </ThemeProvider>
+
+   
+    </>
+    
+}
+
+export default App
+
---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+export const Work = [
+    {
+        id:1,
+        name:"Travel Website",
+        description:"It is build on top of HTML, CSS with styledComponents.",
+        tags:["html","css","styledComponents"],
+        
+        demo:"https://hopeful-kalam-f2dfd0.netlify.app/",
+        github:"https://github.com/aishalg/Travel-Website"
+    },
+    {
+        id:2,
+        name:"VCure: HealthCare",
+        description:"A HealthCare website build in HTML, CSS and BootStrap.",
+        tags:["html","css","bootstrap"],
+        
+        demo:"https://glistening-banoffee-9a1158.netlify.app/",
+        github:"https://github.com/aishalg/Hack36_Website"
+        
+    },
+    {
+        id:3,
+        name:"EduMate",
+        description:"An All in one Educational Hub.",
+        tags:["VueJS","firebase","vuetify"],
+        
+        demo:"http://frosthack-2022.web.app/",
+        github:"https://github.com/aishalg/frosthack-2022"
+    },
+    // {
+    //     id:4,
+    //     name:"Sidebar Menu",
+    //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+    //     tags:["react","styledComponents","react-router"],
+        
+    //     demo:"https://react-sidebar.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-sidebar"
+       
+    // },{
+    //     id:5,
+    //     name:"Todo App(React + Redux)",
+    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     tags:["react","redux","framer-motion"],
+        
+    //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    // },
+    // {
+    //     id:6,
+    //     name:"Responsive Navigation Menu",
+    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     tags:["react","react-router","css"],
+        
+    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    // },
+    
+  
+] 
